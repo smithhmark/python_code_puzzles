2e8421cf82f09dac4bc534f4889252ba107ceee8
Mark Smith <smith.h.mark@gmail.com>
2019-06-27 15:08:48 -0700

    adding a speed up because orig was too slow

--- grids/matrix_rotation.py
+++ grids/matrix_rotation.py
@@ -15,38 +15,50 @@ def ring_bounds(matrix, ring):
         return None, None, None, None
     return top_row_idx, bottom_row_idx, left_col_idx, right_col_idx
 
-def rotate_ring_counterclockwise(matrix, ring):
+def ring_size(top, bottom, left, right):
+    return 2*(bottom-top) + 2 * (right-left)
+
+def rotate_ring_counterclockwise(matrix, ring, N=1):
     tmp = None
     top_row_idx, bottom_row_idx, left_col_idx, right_col_idx = ring_bounds(
             matrix, ring)
-
     if top_row_idx is None:
         return
+    rsz = ring_size(top_row_idx, bottom_row_idx, left_col_idx, right_col_idx)
+    for ii in range(N % rsz):
+        rotate_ring_cw_once(top_row_idx, bottom_row_idx,
+                left_col_idx, right_col_idx, matrix)
+
+def rotate_ring_cw_once(
+        top_row_idx, bottom_row_idx,
+        left_col_idx, right_col_idx,
+        matrix):
+    tmp = None
+    #print(matrix, tmp)
     # down left side
-    print(matrix, tmp)
     for row in range(top_row_idx, bottom_row_idx+1):
         tmp2 = matrix[row][left_col_idx]
         matrix[row][left_col_idx] = tmp
         tmp = tmp2
-    print(matrix, tmp)
+    #print(matrix, tmp)
     # across bottom
     for col in range(left_col_idx+1, right_col_idx+1):
         tmp2 = matrix[bottom_row_idx][col]
         matrix[bottom_row_idx][col] = tmp
         tmp = tmp2
-    print(matrix, tmp)
+    #print(matrix, tmp)
     # up right side
     for row in range(bottom_row_idx - 1, top_row_idx -1, -1):
         tmp2 = matrix[row][right_col_idx]
         matrix[row][right_col_idx] = tmp
         tmp = tmp2
-    print(matrix, tmp)
+    #print(matrix, tmp)
     # back across top
     for col in range(right_col_idx-1, left_col_idx-1, -1):
         tmp2 = matrix[top_row_idx][col]
         matrix[top_row_idx][col] = tmp
         tmp = tmp2
-    print(matrix, tmp)
+    #print(matrix, tmp)
 
 def rotate_ring_clockwise(matrix, ring):
     tmp = None
@@ -75,9 +87,6 @@ def rotate_ring_clockwise(matrix, ring):
 def rotateNcw(matrix, n):
     rings = min(len(matrix)//2, len(matrix[0])//2)
 
-    rots = 0
-    while rots < n:
-        rots += 1
-        for ring in range(rings):
-            rotate_ring_counterclockwise(matrix, ring)
+    for ring in range(rings):
+        rotate_ring_counterclockwise(matrix, ring, n)
     return matrix
--- grids/matrix_rotation_test.py
+++ grids/matrix_rotation_test.py
@@ -45,6 +45,21 @@ def test_rot_ring_ccw():
         [9,6,10,12],
         [13,14,15,16],]
 
+def test_rotate_skinny():
+    matrix = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [10,11,12],
+            ]
+    matrix_rotation.rotateNcw(matrix, 1)
+    assert matrix == [
+            [2,3,6],
+            [1,5,9],
+            [4,8,12],
+            [7,10,11],
+            ]
+
 def test_rotate_cw():
     matrix_rotation.rotateNcw(case_1, 2)
     assert case_1 == expt_1_rot2
